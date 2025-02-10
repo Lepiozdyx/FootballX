3195998aeb72aea0889df4f9171619321c3710c7
Developer <Lepiozdyx@gmail.com>
2025-02-10 10:26:56 -0300

    Initial Commit

---
+++ FootballX/FootballXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballXApp.swift
+//  FootballX
+//
+//  Created by Alex on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
